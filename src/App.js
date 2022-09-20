d25e9045d8036d2d032d966ec3d751cb04be368b
Giovanni Biancuzzo <giovannibiancuzzo.dev@gmail.com>
2022-09-20 19:36:42 +0200

    integrazione

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MyNavbar from "./components/MyNavbar";
 import MyCustomProfile from "./components/MyCustomProfile";
-import ExperienceForm from "./components/ExperienceForm";
-
 import ExperiencesComponent from "./components/ExperiencesComponent";
 
 function App () {
