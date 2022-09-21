08e001469dbdbfe3feb8feadd716b861cfbc9f8c
HamzaMajeed97 <hamzamajeed784@gmail.com>
2022-09-21 17:05:26 +0200

    feature/postcard-feed

--- src/App.js
+++ src/App.js
@@ -7,12 +7,18 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MyNavbar from "./components/MyNavbar";
 import MyCustomProfile from "./components/MyCustomProfile";
 import ExperiencesComponent from "./components/ExperiencesComponent";
+import PostCard from './components/PostCard';
+import MyFeed from './components/MyFeed';
 
 function App () {
   return (
     <BrowserRouter>
       <div className="App">
+
         <MyNavbar />
+        {/* <MyFeed/> */}
+
+        <PostCard/>
         <MyCustomProfile />
         <ExperiencesComponent />
       </div>
