338f73c437b7b3aafdc84d25aef3e0de7a495dd2
Giovanni Biancuzzo <giovannibiancuzzo.dev@gmail.com>
2022-09-23 15:32:19 +0200

    fixe compatibility

--- src/components/SidebarRight.jsx
+++ src/components/SidebarRight.jsx
@@ -3,28 +3,58 @@ import Card from "react-bootstrap/Card";
 import { BsInfoSquareFill } from "react-icons/bs";
 
 function SidebarRight() {
-  return (
-    <Card style={{ width: '20rem', height:'23rem', position: "sticky" , top: "125px" }} className='notiziecard0'>
-      <Card.Body>
-        <Card.Title className='cardttl'> <h5>LinkedIn Notizie</h5>  <BsInfoSquareFill size={13}/></Card.Title>
-        <Card.Text className='notiziecard'>
-          <ul>
-              <li><h6 className='notiziah6'>Ucraina: gli ultimi aggiornamenti</h6>
-                <p>Notizie principali - 11.152 lettori</p>
-                </li>
-              <li><h6 className='notiziah6'>Una nuova pagina per Spotify</h6>
-              <p>2 ore fa</p></li>
-              <li><h6 className='notiziah6'>Lo sprint dei family office italiani</h6>
-              <p>3 ore fa</p></li>
-              <li><h6 className='notiziah6'>Si è inceppata la transizione ecologica</h6>
-              <p> 4 ore fa - 168 lettori</p></li>
-              <li><h6 className='notiziah6'>Eataly passa a Investindustrial</h6>
-              <p>8 ore fa - 368 lettori</p></li>
-          </ul>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card
+            style={{
+                width: "20rem",
+                height: "23rem",
+                position: "sticky",
+                top: "125px",
+            }}
+            className="notiziecard0"
+        >
+            <Card.Body>
+                <Card.Title className="cardttl">
+                    {" "}
+                    <h5>LinkedIn Notizie</h5> <BsInfoSquareFill size={13} />
+                </Card.Title>
+                <Card.Text className="notiziecard" as="div">
+                    <ul>
+                        <li>
+                            <h6 className="notiziah6">
+                                Ucraina: gli ultimi aggiornamenti
+                            </h6>
+                            <p>Notizie principali - 11.152 lettori</p>
+                        </li>
+                        <li>
+                            <h6 className="notiziah6">
+                                Una nuova pagina per Spotify
+                            </h6>
+                            <p>2 ore fa</p>
+                        </li>
+                        <li>
+                            <h6 className="notiziah6">
+                                Lo sprint dei family office italiani
+                            </h6>
+                            <p>3 ore fa</p>
+                        </li>
+                        <li>
+                            <h6 className="notiziah6">
+                                Si è inceppata la transizione ecologica
+                            </h6>
+                            <p> 4 ore fa - 168 lettori</p>
+                        </li>
+                        <li>
+                            <h6 className="notiziah6">
+                                Eataly passa a Investindustrial
+                            </h6>
+                            <p>8 ore fa - 368 lettori</p>
+                        </li>
+                    </ul>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
 }
 
 export default SidebarRight;
